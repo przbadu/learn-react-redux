609983a63bbeb77743d09d2fa291a96ccd4c7ddc
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2017-02-23 10:39:27 +0545

    Add About and Repos component to test routing to them

---
+++ src/Repos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Repos extends Component {
+  render() {
+    return (
+      <div>List of Repos</div>
+    );
+  }
+}
+
+export default Repos;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
+// css
+import './index.css';
+
 // Import components
 import App from './App';
-import './index.css';
+import About from './About';
+import Repos from './Repos';
+
 
 
 const router = (
   <Router history={hashHistory}>
     <Route path="/"  component={App} />
+    <Route path="/repos" component={Repos} />
+    <Route path="/about" component={About} />
   </Router>
 );
 
